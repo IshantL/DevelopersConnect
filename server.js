48abbde8cfb386dff7bef723feb34ea4fd3f1cdc
Ishant L <ishantlambhate@gmail.com>
2019-08-18 11:14:44 +0530

    Mongo DB Conncection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app =express();
+
+//connect DB
+connectDB();
+
 app.get('/',(req,res)=> res.send('API RUNNING'));
 
 // Define Route
