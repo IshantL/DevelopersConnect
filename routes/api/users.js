28eb0deee202c7ef67f9ad9f858f8bd0ba295696
Ishant L <ishantlambhate@gmail.com>
2019-08-18 19:06:04 +0530

    check if user exists

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User')
+
 //@route   POST api/users
 //@desc    Register route
 //@access  Public
 router.post('/',[ check('name', 'Name is required').not().isEmpty(),
   check('email', 'please include a valid email').isEmail(),
   check('password', 'please enter a password with 6 or more characters').isLength({min:6})
-],(req,res)=>{
+],async (req,res)=>{
   const errors =validationResult(req);
   if(!errors.isEmpty()){
     return res.status(400).json({errors: errors.array() });
   }
-  res.send('User Route'),
-    console.log(req.body);
+
+  const { name, email, password } =req.body;
+  try{
+
+    // See if user exists
+    let user = await User.findOne({email});
+    if(user){
+      res.status(400).json({ errors: errors.array() })
+    }
+    // Get user Gravatar
+
+    //Encrypt password
+
+    // return jsonwebtoken
+    res.send('User Route'),
+      console.log(req.body);
+
+  } catch(err){
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+  
 });
 
 module.exports = router;
