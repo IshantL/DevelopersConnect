76f02f9452dbdb1f63e3f72791572e9be4b32a09
IshantL <ishantlambhate@gmail.com>
2019-08-26 17:59:27 +0530

    middleware and jwt verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 //@route   GET api/auth
 //@desc    Test route
 //@access  Public
-router.get('/',auth, (req, res) => res.send('Auth Route'));
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await user.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
